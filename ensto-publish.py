a14afe53f1ef58852bd42032499a82367c7d0d18
juuso <juuso123@iki.fi>
2024-07-29 12:20:29 +0300

    Changed payload to json and only one topic per device.

--- ensto-publish.py
+++ ensto-publish.py
@@ -4,6 +4,8 @@ import logging
 import struct
 import unicodedata
 from pathlib import Path
+from dotenv import load_dotenv, dotenv_values
+import os
 
 import aiomqtt
 from bleak import BleakClient, BleakError
@@ -11,23 +13,32 @@ from bleak import BleakClient, BleakError
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
+# If an environment variable is not found in the .env file, load_dotenv will then search for a variable by the given name in the host environment.
+# loading variables from .env file
+load_dotenv()
+
+config = dotenv_values(".env")
+
 # Define the UUIDs for the characteristics we are interested in (update with correct UUIDs)
 DEVICE_NAME_UUID = "00002a00-0000-1000-8000-00805f9b34fb"  # Example: Device Name
 DATA_UUID = "66ad3e6b-3135-4ada-bb2b-8b22916b21d4"  # Example: Data
 POWER_UUID = "c1686f28-fa1b-4791-9eca-35523fb3597e"  # Example: Power
 
 # Define the factory reset ID characteristic UUID
-FACTORY_RESET_ID_UUID = "f366dddb-ebe2-43ee-83c0-472ded74c8fa"  # Example
+FACTORY_RESET_ID_UUID = "f366dddb-ebe2-43ee-83c0-472ded74c8fa"
 
-# Define MQTT broker details, planning on using .env file
-MQTT_BROKER = "127.0.0.1"
-MQTT_PORT = 1883
-MQTT_CLIENT_ID = "ensto"
 BASE_TOPIC = "ECO16BT"
-MQTT_USERNAME = "user"
-MQTT_PASSWORD = "pass"
 
-ADAPTER_ADDRESS = "hci1" #Your bleuetooth controller hci address
+# Define MQTT broker details, planning on using .env file (dotenv)
+MQTT_BROKER = os.getenv("MQTT_BROKER")
+MQTT_PORT = int(os.getenv("MQTT_PORT"))
+MQTT_CLIENT_ID = os.getenv("MQTT_CLIENT_ID")
+MQTT_USERNAME = os.getenv("MQTT_USERNAME")
+MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
+PAIRING_FILES_PATH = os.getenv("PAIRING_FILES_PATH") or "./"
+
+ADAPTER_ADDRESS = "hci0"  #Your bleuetooth controller hci address
+
 
 def extract_mac_address(filename):
     mac_address = filename[-17:][:12].upper()
@@ -41,22 +52,22 @@ def get_reset_key(filename):
         return bytes(pairing_data["resetCode"])
 
 
-async def mqtt_publish(msgs):
+async def mqtt_publish(topic, msg):
     try:
         async with aiomqtt.Client(hostname=MQTT_BROKER, port=MQTT_PORT, identifier=MQTT_CLIENT_ID,
                                   username=MQTT_USERNAME, password=MQTT_PASSWORD) as client:
-            for msg in msgs:
-                await client.publish(topic=msg[0], payload=msg[1])
-                #log.info(f"published to {msg[0]}, payload:{msg[1]}")
 
-            log.info("published")
+            await client.publish(topic, msg)
+            log.info(f"published to {topic}")
+            log.info(f"msg: {msg}")
+
     except BleakError as e:
         log.error(f"Bleak error. {e}")
     except Exception as e:
         log.error(f"MQTT publish failed. {e}")
 
 
-async def connect(thermostat_address, reset_key):
+async def connect(thermostat_address, reset_key, device_name):
     max_retries = 3
     retry_delay = 20  # seconds
 
@@ -75,21 +86,12 @@ async def connect(thermostat_address, reset_key):
                 #print(f"reset key: {factory_reset_id}")
 
                 # Read characteristics
-                device_name = await client.read_gatt_char(DEVICE_NAME_UUID)
                 data_bytes = await client.read_gatt_char(DATA_UUID)
                 #power_bytes = await client.read_gatt_char(DATA_UUID)
 
-                #device_name_trimmed = bytes(device_name.rstrip(b'\x00'))
-                #print(f"Device Name trimmed: {device_name_trimmed}")
-                # Find the index of the first null byte
-                device_name_null_index = device_name.find(b'\x00')
-
-                # Slice the byte sequence up to the first null byte
-                device_name_utf8_bytes = device_name[:device_name_null_index]
                 device_name = unicodedata.normalize('NFKD',
-                                                    device_name_utf8_bytes.decode('utf-8', errors='ignore')).encode(
+                                                    device_name).encode(
                     'ascii', 'ignore').decode('utf-8').lower().replace(' ', '')
-                log.info(f"Device Name: {device_name_utf8_bytes.decode('utf-8', errors='ignore')}")
 
                 # Decode each field according to the specified structure
                 temp_setting_target = struct.unpack('<H', data_bytes[1:3])[0] * (45.0 / 450)
@@ -106,37 +108,37 @@ async def connect(thermostat_address, reset_key):
                 potentiometer_value = data_bytes[20]  # A
 
                 # Print decoded values
-                log.info(f"Temp setting (target): {temp_setting_target:.2f}   C")
-                log.info(f"Temp setting %: {temp_setting_percent}%")
-                log.info(f"Room temperature: {room_temperature:.2f}   C")
-                log.info(f"Floor temperature: {floor_temperature:.2f}   C")
-                log.info(f"Active relay state: {'On' if active_relay_state == 1 else 'Off'}")
-                log.info(f"Alarm code: {alarm_code}")
-                log.info(f"Active mode: {active_mode}")
-                log.info(f"Active heating mode: {active_heating_mode}")
-                log.info(f"Boost mode enabled: {'Enabled' if boost_mode_enabled == 1 else 'Disabled'}")
-                log.info(f"Boost mode setpoint: {boost_mode_setpoint_minutes} minutes")
-                log.info(f"Boost mode remaining time: {boost_mode_remaining_minutes} minutes")
-                log.info(f"Potentiometer value: {potentiometer_value}%")
+                log.debug(f"Temp setting (target): {temp_setting_target:.2f}   C")
+                log.debug(f"Temp setting %: {temp_setting_percent}%")
+                log.debug(f"Room temperature: {room_temperature:.2f}   C")
+                log.debug(f"Floor temperature: {floor_temperature:.2f}   C")
+                log.debug(f"Active relay state: {'On' if active_relay_state == 1 else 'Off'}")
+                log.debug(f"Alarm code: {alarm_code}")
+                log.debug(f"Active mode: {active_mode}")
+                log.debug(f"Active heating mode: {active_heating_mode}")
+                log.debug(f"Boost mode enabled: {'Enabled' if boost_mode_enabled == 1 else 'Disabled'}")
+                log.debug(f"Boost mode setpoint: {boost_mode_setpoint_minutes} minutes")
+                log.debug(f"Boost mode remaining time: {boost_mode_remaining_minutes} minutes")
+                log.debug(f"Potentiometer value: {potentiometer_value}%")
 
                 #log.debug(f"Power: {power_bytes}")
 
-                msgs = [(BASE_TOPIC + device_name + "/room/temperature", round(room_temperature, 2)),
-                        (BASE_TOPIC + device_name + "/target/temperature", round(temp_setting_target, 2)),
-                        (BASE_TOPIC + device_name + "/floor/temperature", round(floor_temperature, 2)),
-                        (BASE_TOPIC + device_name + "/heat", active_relay_state),
-                        (BASE_TOPIC + device_name + "/boost/enabled", boost_mode_enabled),
-                        (BASE_TOPIC + device_name + "/boost/setpoint", boost_mode_setpoint_minutes),
-                        (BASE_TOPIC + device_name + "/boost/remaining", boost_mode_remaining_minutes)]
+                json_dict = {"roomTemperature": round(room_temperature, 2),
+                             "targetTemperature": round(temp_setting_target, 2),
+                             "floorTemperature": round(floor_temperature, 2),
+                             "heat": bool(active_relay_state),
+                             "boostEnabled": bool(boost_mode_enabled),
+                             "boostSetpointMinutes": boost_mode_setpoint_minutes,
+                             "boostRemainingMinutes": boost_mode_remaining_minutes}
 
-                await mqtt_publish(msgs)
-
-                log.debug(f"msgs: {msgs}")
+                json_msg = json.dumps(json_dict)
+                topic = BASE_TOPIC + "/" + device_name
+                await mqtt_publish(topic, json_msg)
 
                 return  #exit if successful
 
         except Exception as e:
-            log.warning(f"Attempt {attempt} failed with error: {e}")
+            log.warning(f"Attempt {attempt} failed with error: {e}",e)
             if attempt < max_retries:
                 log.info(f"Retrying in {retry_delay} seconds...")
                 await asyncio.sleep(retry_delay)
@@ -146,17 +148,20 @@ async def connect(thermostat_address, reset_key):
 
 
 async def main():
-    directory_in_str = "./" # change where you have your pairing files
-    pathlist = Path(directory_in_str).rglob('pairing-*.json')
-    for path in pathlist:
-        # because path is object not string
-        pairing_file = str(path)
-        thermostat_address = extract_mac_address(pairing_file)
-        log.info(f"Connecting to {thermostat_address}")
-        reset_key = get_reset_key(path)
-        log.debug(f"reset_key:{reset_key}")
+    pairing_files = Path(PAIRING_FILES_PATH).glob('pairing-*.json')
+    for file in pairing_files:
+        if str(file) != 'pairing-000000000000.json':
+            # because path is object not string
+            pairing_file = str(file)
+            thermostat_address = extract_mac_address(pairing_file)
+            log.info(f"Connecting to {thermostat_address}")
+            with open(pairing_file) as f:
+                pairing_data = json.load(f)
 
-        await connect(thermostat_address, reset_key)
+            reset_key = bytes(pairing_data["resetCode"])
+            device_name = pairing_data["deviceName"]
+
+            await connect(thermostat_address, reset_key, device_name)
 
 
 if __name__ == "__main__":
